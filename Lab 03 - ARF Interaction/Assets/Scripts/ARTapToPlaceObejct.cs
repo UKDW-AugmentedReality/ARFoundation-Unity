f2a90f90562f73d17229874249c84f49e363392d
didanendya <93307825+didanendya-tiukdw@users.noreply.github.com>
2023-05-15 23:46:20 +0700

    Lab 03
    Updated

---
+++ Lab 03 - ARF Interaction/Assets/Scripts/ARTapToPlaceObejct.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System;
+
+public class ARTapToPlaceObejct : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+
+    private Pose PlacementPose;
+    private ARRaycastManager aRRaycastManager;
+    private bool placementPosIsValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if(placementPosIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObejct();
+        }    
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if(placementPosIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPosIsValid = hits.Count > 0;
+        if(placementPosIsValid)
+        {
+            PlacementPose = hits[0].pose;
+        }
+    }
+
+    private void PlaceObejct()
+    {
+        Instantiate(objectToPlace, PlacementPose.position, PlacementPose.rotation);
+    }
+
+   
+}
+
